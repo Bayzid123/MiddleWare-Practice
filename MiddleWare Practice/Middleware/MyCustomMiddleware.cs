43e7231031ae7c79d7f7c9dbaca807cd8621db0d
Md. Bayzid <58081826+Bayzid123@users.noreply.github.com>
2022-01-04 10:35:50 +0600

    Add project files.

---
+++ MiddleWare Practice/Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddleWare_Practice.Middleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.Headers.Add("Middleware", "Middleware Header");
+            await next(context);
+        }
+    }
+}
---
+++ MiddleWare Practice/MiddlewareCustom.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MiddleWare_Practice
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MiddlewareCustom
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public MiddlewareCustom(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger("MiddlewareCustom");
+            _logger.LogInformation("This is my Information");
+            _logger.LogError("This is an error");
+            _logger.LogWarning("This is a warning");
+            _logger.BeginScope("This is a beingscope");
+            _logger.LogDebug("This is a debug process");
+
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("Executed");
+            await _next.Invoke(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareCustomExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareCustom(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareCustom>();
+        }
+    }
+}
